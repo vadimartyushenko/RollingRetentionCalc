fde897c6c070b206bf8402a813f332ae1bd36cb7
Вадим Артюшенко <artushenkomail@mail.ru>
2021-09-05 20:11:53 +0700

    add  project DBRepository and Models

---
+++ Models/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+	public class User
+	{
+		public decimal UserId { get; set; }
+
+		public DateTime RegistrationData { get; set; }
+
+		public DateTime LastActivityDate { get; set; }
+	}
+}
